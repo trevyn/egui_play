9c6a23922bfb96cf25b3bc23fe86a10616cd0778
trevyn <230691+trevyn@users.noreply.github.com>
2022-04-05 13:29:09 +0300

    remove persistence feature

--- src/app.rs
+++ src/app.rs
@@ -5,9 +5,6 @@ struct Element {
  fontsize: f32,
 }
 
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
-#[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
 #[derive(Default)]
 pub struct TemplateApp {
  elements: Vec<Element>,
@@ -27,19 +24,6 @@ impl epi::App for TemplateApp {
   _storage: Option<&dyn epi::Storage>,
  ) {
   ctx.set_pixels_per_point(3.0);
-  // Load previous app state (if any).
-  // Note that you must enable the `persistence` feature for this to work.
-  #[cfg(feature = "persistence")]
-  if let Some(storage) = _storage {
-   *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
-  }
- }
-
- /// Called by the frame work to save state before shutdown.
- /// Note that you must enable the `persistence` feature for this to work.
- #[cfg(feature = "persistence")]
- fn save(&mut self, storage: &mut dyn epi::Storage) {
-  epi::set_value(storage, epi::APP_KEY, self);
  }
 
  /// Called each time the UI needs repainting, which may be many times per second.
